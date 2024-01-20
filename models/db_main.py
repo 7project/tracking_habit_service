1774df65ab35ad6d6a4dafcf90dbaf67b21a470f
User <housechel@mail.ru>
2024-01-20 19:22:26 +0300

    add code to file db_main.py

--- models/db_main.py
+++ models/db_main.py
@@ -1,3 +1,19 @@
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+from settings import DATABASE_URL
 
 
-db_main = ...
+class Database:
+    def __init__(self):
+        self.engine = create_async_engine(
+            url=DATABASE_URL,
+            echo=False,
+        )
+        self.session_dependency = async_sessionmaker(
+            bind=self.engine,
+            autoflush=False,
+            autocommit=False,
+            expire_on_commit=False,
+        )
+
+
+db_main = Database()
