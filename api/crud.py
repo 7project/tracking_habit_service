9ceaf410c43855afda370d501b11af2297b303b4
User <housechel@mail.ru>
2024-01-23 17:38:59 +0300

    edit base crud specificate logic func

--- api/crud.py
+++ api/crud.py
@@ -1,7 +1,15 @@
 
-def create_user(user_in, session):
+async def create_user(user_in, session):
     ...
 
 
-def get_user_to_telegram_id(session, telegram_id):
+async def get_user_to_telegram_id(session, telegram_id):
+    ...
+
+
+async def get_habits_for_user_id(session, user_id):
+    ...
+
+
+async def get_habit_tracking_for_habit_id(session, habit_id):
     ...
