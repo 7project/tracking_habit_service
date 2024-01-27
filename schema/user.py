d580ee147959c19fef21baa5c6cdc3eef0602b65
User <housechel@mail.ru>
2024-01-27 18:39:53 +0300

    add schemy UserBase User CreateUser

--- schema/user.py
+++ schema/user.py
@@ -1,22 +1,4 @@
-from pydantic import BaseModel
-
-
-class CreateUser(BaseModel):
-    ...
-
-
-class User(BaseModel):
-    ...
-
-
-class UserBase:
-    ...
-
-
-class UserSchema(BaseModel):
-    name: str
-    telegram_id: int
-    is_active: bool
+from pydantic import BaseModel, Field
 
 
 class HabitSchemy(BaseModel):
@@ -29,4 +11,25 @@ class HabitTrackingSchema(BaseModel):
 
 class UserOut(BaseModel):
     ...
+
+
+class UserBase(BaseModel):
+    telegram_id: int
+
+
+class CreateUser(UserBase):
+    name: str
+    password: str
+
+
+class User(UserBase):
+    id: int
+    name: str
+    is_active: bool
+
+
+class UserSchema(BaseModel):
+    name: str
+    telegram_id: int
+    is_active: bool
 
