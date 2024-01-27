0e6a8090ac87d3c1d8ddb7123728adb26e7685dd
User <housechel@mail.ru>
2024-01-27 18:42:01 +0300

    add to file crud async func create user

--- api/crud.py
+++ api/crud.py
@@ -1,6 +1,23 @@
+from sqlalchemy.ext.asyncio import AsyncSession
+from models import User
+from schema.user import CreateUser
+from services.jwt import hash_password
 
-async def create_user(user_in, session):
-    ...
+
+async def create_user(user_in: CreateUser, session: AsyncSession):
+    hashed_password = hash_password(user_in.password)
+    user = User(
+        name=user_in.name,
+        telegram_id=user_in.telegram_id,
+        password=hashed_password,
+        is_active=True,
+
+
+    )
+    session.add(user)
+    await session.commit()
+    await session.refresh(user)
+    return user
 
 
 async def get_user_to_telegram_id(session, telegram_id):
