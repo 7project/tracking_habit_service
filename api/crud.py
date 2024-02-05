0a8a8d0bf0f7c9043cc82b6034a27ec8849949e6
User <housechel@mail.ru>
2024-02-05 21:27:29 +0300

    add logic func get_habits_for_user_id

--- api/crud.py
+++ api/crud.py
@@ -1,6 +1,8 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select, Result
-from models import User
+from sqlalchemy.orm import selectinload
+
+from models import User, Habit, HabitTracking
 from fastapi import status
 from schema.user import CreateUser
 from services.jwt import hash_password
@@ -50,8 +52,34 @@ async def get_user_to_telegram_id(session: AsyncSession, telegram_id: int):
 
 
 async def get_habits_for_user_id(session, user_id):
-    ...
+
+    unauthorized_exp = HTTPException(
+        status_code=status.HTTP_409_CONFLICT,
+        detail="Invalid request"
+    )
+    statement = (select(Habit)
+                 .options(selectinload(Habit.tracking),)
+                 .where(Habit.user_id == user_id))
+    result: Result = await session.execute(statement)
+    habits = result.scalars()
+
+    if habits is not None:
+        raise unauthorized_exp
+
+    return habits
 
 
 async def get_habit_tracking_for_habit_id(session, habit_id):
-    ...
+    unauthorized_exp = HTTPException(
+        status_code=status.HTTP_409_CONFLICT,
+        detail="Invalid request"
+    )
+    statement = (select(HabitTracking)
+                 .where(HabitTracking.habit_id == habit_id))
+    result: Result = await session.execute(statement)
+    habit_tracking = result.scalar_one_or_none()
+
+    if habit_tracking is not None:
+        raise unauthorized_exp
+
+    return habit_tracking
