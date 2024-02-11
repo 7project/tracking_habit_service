1906b4a5c959e8145cb186e72541385cc06ca518
User <housechel@mail.ru>
2024-02-11 17:06:48 +0300

    change name sert to cert

--- settings.py
+++ settings.py
@@ -16,8 +16,8 @@ DB_PASS = env("DB_PASS")
 DATABASE_URL = env("DATABASE_URL")
 
 
-path_private_key: Path = Path(__file__).parent / "sert" / "jwt-private.pem"
-path_public_key: Path = Path(__file__).parent / "sert" / "jwt-public.pem"
+path_private_key: Path = Path(__file__).parent / "cert" / "jwt-private.pem"
+path_public_key: Path = Path(__file__).parent / "cert" / "jwt-public.pem"
 algorithm = "RS256"
 expire_minutes = 35
 # expire_timedelta = 5
