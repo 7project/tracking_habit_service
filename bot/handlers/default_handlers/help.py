8472bf8b564bb42c5fa9054b1749f006162a7aca
User <housechel@mail.ru>
2024-12-12 14:14:46 +0300

    corrected help.py

--- bot/handlers/default_handlers/help.py
+++ bot/handlers/default_handlers/help.py
@@ -8,12 +8,7 @@ from loader import bot
 def bot_help(message: Message):
     text = [f"/{command} - {desk}" for command, desk in DEFAULT_COMMANDS]
     bot.reply_to(message, "\n".join(text))
-    bot.send_message(message.chat.id, f"Сервис по треку полезных привычек находится в стадии разработки.\n"
-                                      f"Функционал и логика на текущий момент дорабатывается.\n"
+    bot.send_message(message.chat.id, f"Сервис по трекингу\выполнения полезных привычек.\n"
                                       f"Время оповещения о ваших привычках /habits.\n"
-                                      f"В среднем в это время: 08:15 10:15 12:15 15:15 18:15 20:15 МСК.\n"
-                                      f"Примерное время запуска бота ~9:00-20:00. МСК.\n"
-                                      f"На текущий момент запускается в ручную для теста.")
-
-    # bot.send_message(message.chat.id, f"Сервис по трекингу полезных привычек.\n"
-    #                                   f"/habits.")
+                                      f"Фиксировано: 08:15 10:15 12:15 15:15 18:15 20:15 МСК.\n"
+                                      f"Текущая версия: 1.0.0")
